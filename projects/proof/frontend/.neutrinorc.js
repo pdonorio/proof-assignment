26062e75022da3d96d56d67973f20e3794810afc
Paolo D'Onorio De Meo <p.donorio.demeo@gmail.it>
2018-08-11 01:24:58 +0200

    Adding base neutrino scaffold

--- projects/proof/frontend/.neutrinorc.js
+++ projects/proof/frontend/.neutrinorc.js
@@ -1,65 +1,14 @@
 module.exports = {
   use: [
-    ['@neutrinojs/airbnb', {
-      include: [ './src', './test'],
-      eslint: {
-        rules: {
-          "jsx-a11y/anchor-is-valid": [ "error", {
-            "components": [ "Link" ],
-            "specialLink": [ "to", "hrefLeft", "hrefRight" ],
-            "aspects": [ "noHref", "invalidHref", "preferButton" ]
-          }],
-          "jsx-a11y/label-has-for": [
-            "error", {
-              "components": [ "label"  ],
-              "required": {
-                "every": [ "id" ]
-              }
-            }
-          ],
-          'import/extensions': [
-            "error", "ignorePackages"
-          ],
-          'react/forbid-prop-types': 'off',
-          'react/jsx-boolean-value': 'off',
-          'react/no-array-index-key': 'off'
-        },
-      }
-    }
-    ],
+    '@neutrinojs/airbnb',
     [
       '@neutrinojs/react',
       {
         html: {
-          title: 'Proof - The game',
-          links: [
-            {
-              href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
-              rel: 'stylesheet'
-            },
-          ]
-        },
-        style: {
-          test: /\.(css|sass|scss)$/,
-          modulesTest: /\.module\.(css|sass|scss)$/,
-          loaders: [
-            {
-              loader: 'sass-loader',
-              useId: 'sass'
-            }
-          ]
-        },
-        env: [
-          'API_URL',
-        ],
+          title: 'some'
+        }
       }
     ],
-    ['@neutrinojs/jest', {
-      setupFiles: [
-        '<rootDir>/test/setup.js',
-      ],
-      snapshotSerializers: ['enzyme-to-json/serializer']
-    }],
-    neutrino => neutrino.config.output.publicPath('/')
+    '@neutrinojs/jest'
   ]
 };
--- projects/proof/frontend/src/App.jsx
+++ projects/proof/frontend/src/App.jsx
@@ -1,31 +1,16 @@
-import React from 'react';
-import { PropTypes } from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Quizzes from './containers/Quizzes';
-import Navbar from './components/Navbar';
-import NewQuiz from './containers/NewQuiz';
+import { Component } from 'react';
+import './App.css';
 
-const styles = theme => ({
-  wrapper: {
-    backgroundColor: theme.palette.tertiary.main,
-    height: '100vh',
-  },
-});
+export default class App extends Component {
+  state = {
+    name: 'some',
+  };
 
-const App = ({ classes }) => (
-  <BrowserRouter>
-    <div className={classes.wrapper}>
-      <Navbar />
-      <Route exact path="/" component={Quizzes} />
-      <Route exact path="/quizzes" component={Quizzes} />
-      <Route exact path="/quizzes/new" component={NewQuiz} />
-    </div>
-  </BrowserRouter>
-);
-
-App.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(App);
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to {this.state.name}</h1>
+      </div>
+    );
+  }
+}

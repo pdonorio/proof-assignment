30b38b845e0cf029c92120ecef0ef00a2abbd627
Matthew Carrion <matthewccarrion@gmail.com>
2018-08-16 23:27:28 -0500

    Finishing up broad outlines of the project

--- projects/proof/backend/models/mongo.py
+++ projects/proof/backend/models/mongo.py
@@ -3,11 +3,9 @@
 """
 https://pymodm.readthedocs.io/en/0.4.0/getting-started.html#defining-models
 """
-import requests
-import html2text
-
-from lxml import html
+from BeautifulSoup import BeautifulSoup
 from pymodm import MongoModel, fields
+from urllib.request import urlopen
 
 from proof.apis import APP_DB
 
@@ -24,30 +22,46 @@ class ProofArticle(MongoModel):
 
     These fields are scraped and parsed from articles users post to Proof.
     """
-    author = fields.CharField(primary_key=True, blank=True)
+    author = fields.CharField(primary_key=True)
     truth = fields.BooleanField(blank=True)  # Mostly True / Mostly False
-    title = fields.CharField(unique=True, blank=True)
-    text = fields.CharField(blank=True)
-    date = fields.DateTimeField(blank=True)
+    title = fields.CharField(unique=True)
+    text = fields.CharField()
+    date = fields.DateTimeField()
     value = fields.IntegerField(default=1)
     url = fields.URLField()
 
-    def save(self, *args, **kwargs):
+    class Meta:
+        connection_alias = APP_DB
+
+    def __init__(self, *args, **kwargs):
         self.text = self.scrape_text()
         self.title = self.parse_title()
         self.author = self.parse_name()
         self.date = self.parse_date()
-        super().save(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def scrape_text(self):
-        webpage = requests.get(self.url)
+        page = urllib.request.urlopen(self.url)
+        html = webpage.read()
+        page.close()
+        soup = BeautifulSoup(html)
+        scraped_text = ''.join(soup.findAll(text=True))
+        return scraped_text
 
     def parse_title(self):
+        page = urllib.request.urlopen(self.url)
+        soup = BeautifulSoup(page)
+        parse_title = soup.title.string
+        return parse_title
 
     def parse_name(self):
+       soup = BeautifulSoup(requests.get(s).content, 'html.parser')
+       meta = soup.find('meta', {'name': 'byl'})
+       parse_name = meta["content"]
+       return parse_name
 
     def parse_date(self):
-
-
-    class Meta:
-        connection_alias = APP_DB
+       soup = BeautifulSoup(requests.get(s).content, 'html.parser')
+       meta = soup.find('meta', {'name': 'byl'})
+       parse_date = meta["content"]
+       return parse_date
--- projects/proof/notes/todo.md
+++ projects/proof/notes/todo.md
@@ -4,10 +4,9 @@
 You can keep track of the things you need to do in this markdown file.
 
 - [x] Create list of tasks
-- [ ] Create a form for uploading a url
-- [ ] Create a function for parsing the url
-- [ ] Create a function for reading the contents of the webpage that the url directs to
-- [ ] Parse out authors, date, title, source, etc.
-- [ ] Take parsed out metadata/text and have it fill out fields in a predefined model
-- [ ] Add checks for duplicate titles and urls
-- [ ] Add checks for similar content
+- [x] Create a form for uploading a url
+- [x] Create a function for parsing the url
+- [x] Create a function for reading the contents of the webpage that the url directs to
+- [x] Parse out authors, date, title, source, etc.
+- [x] Take parsed out metadata/text and have it fill out fields in a predefined model
+- [x] Add checks for duplicate urls

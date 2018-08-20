c5f5ec37464e02675b9f60d00513f1aec359aab5
Your Name <you@example.com>
2018-08-20 16:50:12 +0200

    Should be everything.

--- projects/proof/backend/apis/article.py
+++ projects/proof/backend/apis/article.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 
-from restapi.rest.definition import EndpointResource
+import json
+from difflib import SequenceMatcher
+from urllib.parse import urlparse
+
+# MY imports
+import pymodm.errors
+import pymongo
+import requests
+from flask import request
+from newspaper import Article
 from proof.apis import SERVICE_NAME
-# from utilities import htmlcodes as hcodes
+from restapi.rest.definition import EndpointResource
 from utilities.logs import get_logger
 
 log = get_logger(__name__)
@@ -10,7 +19,7 @@ log = get_logger(__name__)
 
 class Articles(EndpointResource):
 
-    def get(self, url=None):
+    def get(self):
         """
         Get an article if requested,
         or the top 10 articles if no url is specified
@@ -22,16 +31,21 @@ class Articles(EndpointResource):
         mongo = self.get_service_instance(SERVICE_NAME)
         log.debug('Mongo ODM handler: %s', mongo)
 
-        mongo.Examples.objects.all()
-        # custom ODM models (like `Examples`) can be added in
-        # projects/proof/backend/models/mongo.py
-        # documentation for queries at:
-        # https://pymodm.readthedocs.io/en/0.4.0
-        #   /getting-started.html#accessing-data
+        try:
+            # If the url variable is passed on the GET request return only the one article.
+            inputs = self.get_input()
+            url = inputs['url']
+            # Does the url exist in our database?
+            try:
+                data = mongo.Article_M.objects.get({'_id': url}).to_son()
+            except pymodm.errors.DoesNotExist:
+                return self.send_errors(message='The url you entered does not exist in our database.')
 
-        # you can safely return python built-in types
-        # that can be encoded with the json library
-        return 'To be implemented'
+        except KeyError:
+            # If url isn't passed return top 10 as determined by the limit function.
+            data = list(mongo.Article_M.objects.values().all().limit(10))
+
+        return json.dumps(data, default=str)
 
     def post(self):
         """
@@ -39,18 +53,76 @@ class Articles(EndpointResource):
         """
 
         log.info('Request: submitting an article for parsing')
+        # connect to mongo
+        mongo = self.get_service_instance(SERVICE_NAME)
 
         # Try to get input
-        inputs = self.get_input()
-        log.pp(inputs)
+        try:
+            inputs = self.get_input()
+            url = inputs['url']
+        except KeyError:
+            return self.send_errors(message='You must submit a url for parsing!')
 
-        # if url is None:
-        #     return self.send_errors(
-        #         message='You must submit a url for parsing!'
-        #     )
+        # Use the newspaper library to get all the scraped info.
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
 
-        # to store data on mongo via ODM:
-        # https://pymodm.readthedocs.io/en/0.4.0
-        #   /getting-started.html#creating-data
+        # Get the base url
+        parsed_uri = urlparse(url)
+        origin = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+        data = requests.get("http://ipinfo.io/json").json()
+        flagged = False
+        # Check for plagiarism. Very inefficient. Using indexes will make this a lot faster probably.
+        for art in mongo.Article_M.objects.all():
+            title_ratio = SequenceMatcher(None, art.title, article.title).ratio()
+            if title_ratio > 0.95:
+                return self.send_errors(message='An article with a very similar title has already been posted.')
+            text_ratio = SequenceMatcher(None, art.text, article.text).ratio()
+            if text_ratio > 0.8:
+                # We don't inform the user of his transgression.
+                flagged = True
+                break
 
-        return 'To be implemented'
+        # Save to database if not already in there.
+        art_obj = self.save_to_db(article, mongo, origin, data, url, flagged)
+
+        return art_obj.to_son()
+
+    def save_to_db(self, article, mongo, origin, data, url, flagged):
+        """ Save to database if it doesn't exit already."""
+        # Check for empty lists
+        authors, movies, images, keywords = self.determine_if_lists_are_empty(article)
+        try:
+            art_obj = mongo.Article_M(url=url, title=article.title, authors=authors, publish_date=article.publish_date,
+                                      text=article.text, keywords=keywords, summary=article.summary, source=origin,
+                                      movies=movies, images=images, ip=request.remote_addr, city=data['city'],
+                                      country=data['country'], region=data['region'], flagged=flagged).save(
+                force_insert=True)
+        except pymongo.errors.DuplicateKeyError:
+            return self.send_errors(message='This article already exists.')
+        return art_obj
+
+    @staticmethod
+    def determine_if_lists_are_empty(article):
+        """ Determine if the lists returned by the newspaper library are empty.
+        If they are, we pass a default not found string to the database."""
+        if not article.authors:
+            authors = ['Not Found.']
+        else:
+            authors = article.authors
+        if not article.keywords:
+            keywords = ['Not Found.']
+        else:
+            keywords = article.keywords
+        if not article.images:
+            images = ['Not Found.']
+        else:
+            images = article.images
+        if not article.movies:
+            movies = ['Not Found.']
+        else:
+            movies = article.authors
+
+        return authors, movies, images, keywords
--- projects/proof/backend/models/mongo.py
+++ projects/proof/backend/models/mongo.py
@@ -5,6 +5,8 @@ https://pymodm.readthedocs.io/en/0.4.0/getting-started.html#defining-models
 """
 
 from pymodm import MongoModel, fields
+from pymongo import TEXT
+from pymongo.operations import IndexModel
 from proof.apis import APP_DB
 
 
@@ -23,6 +25,28 @@ class Examples(MongoModel):
     description = fields.CharField()
     created = fields.DateTimeField()
     value = fields.IntegerField(default=1)
+
+    class Meta:
+        connection_alias = APP_DB
+
+
+class Article_M(MongoModel):
+    url = fields.CharField(primary_key=True)
+    title = fields.CharField()
+    authors = fields.ListField(fields.CharField(), default=list)
+    publish_date = fields.DateTimeField()
+    text = fields.CharField()
+    keywords = fields.ListField(fields.CharField())
+    summary = fields.CharField()
+    source = fields.CharField()
+    html = fields.CharField()
+    ip = fields.CharField()
+    city = fields.CharField()
+    country = fields.CharField()
+    region = fields.CharField()
+    images = fields.ListField(fields.CharField(), default=list)
+    movies = fields.ListField(fields.CharField(), default=list)
+    flagged = fields.BooleanField(default=False)
 
     class Meta:
         connection_alias = APP_DB
